49876ef2ae37eb77dbc369ba0784c5d74437926a
Joseph <67434468+Illigo19@users.noreply.github.com>
2021-04-04 20:22:35 +0200

    added "gamer" mode and Version 1.5
    fixed some bugs related to checkboxes, new priorities in color.js, added "gamer" mode (color.js and background.js, by Yazil), changed the version (from 1.0.0.0 to 1.5.0.0), added a banner for opera Add-on (300*188p)

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var colorWell
+var colorWellBack
 
 
 
@@ -15,6 +15,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
       if (result.check == 'check') {
         document.getElementById("backTrue").checked = true;
       }
+  });
+
+  chrome.storage.local.get(['checkStudent'], function(result) {
+    
+    if (result.checkStudent == 'check') {
+      document.getElementById("backTrueStudent").checked = true;
+    }
+  });
+
+  chrome.storage.local.get(['gamer'], function(result) {
+    
+    if (result.gamer == 'check') {
+      document.getElementById("gamer").checked = true;
+    }
   });
 
 //save state of checkbox
@@ -55,10 +69,111 @@ document.addEventListener('DOMContentLoaded', (event) => {
       console.log('Value currently is ' + stateCheck);
 
     });
-
+//second checkbox
     })
 
-  });
+    var checkbox = document.getElementById("backTrueStudent");
+    checkbox.addEventListener('change', function() {
+      if (document.getElementById("backTrueStudent").checked) {
+        checked = "check";
+        chrome.storage.local.set({'checkStudent': checked}, function() {
+          console.log('is checked');
+        });
+
+        chrome.storage.local.get(['studBack'], function(result) {
+          colorStudDefault = result.studBack;
+          console.log(colorStudDefault);
+        
+        });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {type: "student", color: colorStudDefault});
+          });
+
+      } else {
+        checked = "not";
+        chrome.storage.local.set({'checkStudent': checked}, function() {
+          console.log('not checked');
+        });
+        //set background-color to #ffffff
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {type: "student", color: "#0E4F94"});
+        });
+
+      }
+      // for console only
+      chrome.storage.local.get(['checkStudent'], function(result) {
+        stateCheck = result.checkStudent;
+        console.log('Value currently is ' + stateCheck);
+      });
+    })
+  //all checkBox
+    var checkbox = document.getElementById("all");
+    checkbox.addEventListener('change', function() {
+
+
+      if (document.getElementById("all").checked == false) {
+
+        
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {type: "student", color: "#0E4F94"});
+        });
+        document.getElementById("backTrueStudent").checked = false;
+        document.getElementById("backTrue").checked = false;
+        document.getElementById("gamer").checked = false;
+        checked = "not";
+        chrome.storage.local.set({'gamer': checked}, function() {
+          console.log('not checked');
+        });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {type: "body", color: "#ffffff"});
+        });
+      }
+    });
+    //gamer's mod
+    var checkbox = document.getElementById("gamer");
+    checkbox.addEventListener('change', function() {
+      if (checkbox.checked){
+
+        document.getElementById("backTrueStudent").checked = false;
+        checked = "not";
+        chrome.storage.local.set({'checkStudent': checked}, function() {
+          console.log('not checked');
+        });
+        document.getElementById("backTrue").checked = false;
+        checked = "not";
+        chrome.storage.local.set({'check': checked}, function() {
+          console.log('not checked');
+        });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {type: "body", color: "#ffffff"});
+        });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {type: "student", color: "#0E4F94"});
+        });
+        checked = "check";
+        chrome.storage.local.set({'gamer': checked}, function() {
+          console.log('is checked');
+        });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {type: "gamer", color: "on"});
+        });
+      } else {
+        checked = "not";
+        chrome.storage.local.set({'gamer': checked}, function() {
+          console.log('not checked');
+        });
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {type: "gamer", color: "off"});
+        });
+      }
+      
+    });
+  
+});
+
+
+
 
 
 
@@ -69,24 +184,86 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 function startup() {
-  colorWell = document.querySelector("#backcolor");
+  colorWellBack = document.querySelector("#backcolor");
+  colorStud = document.querySelector("#elevBack")
 
 
   chrome.storage.local.get(['key'], function(result) {
     defaultColor = result.key;
     console.log('Value currently is ' + defaultColor);
-    colorWell.value = defaultColor;
-    colorWell.style.background = defaultColor;
-    colorWell.addEventListener("input", updateFirst, false);
-    colorWell.addEventListener("change", updateAll, false);
-    colorWell.select();
-
+    colorWellBack.value = defaultColor;
+    colorWellBack.style.background = defaultColor;
+    colorWellBack.addEventListener("input", updateFirst, false);
+    colorWellBack.addEventListener("change", updateAll, false);
+    colorWellBack.select();
   });
+
+  chrome.storage.local.get(['studBack'], function(result) {
+    colorStudDefault = result.studBack;
+    console.log("it's"+colorStud);
+
+    colorStud.value = colorStudDefault;
+    colorStud.style.background = colorStudDefault;
+    colorStud.addEventListener("input", updateStudentFirst, false);
+    colorStud.addEventListener("change", updateStudentAll, false);
+    colorStud.select();
+
+
+  })
 }
   
 
 
-  
+function updateStudentFirst(event) {
+  var student = document.querySelector("#elevBack");
+
+  if (student) {
+    
+    student.style.background = event.target.value;
+    color = event.target.value;
+
+    if (document.getElementById("backTrueStudent").checked) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: "student", color: event.target.value});
+      });
+
+      //save state of color
+      chrome.storage.local.set({studBack: color}, function() {
+        console.log('Value is set to ' + color);
+
+      });
+
+
+    }else {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: "student", color: "#0E4F94"});
+      });
+      
+    }
+    
+  }
+}
+
+function updateStudentAll(event) {
+  document.querySelectorAll("#elevBack").forEach(function(student) {
+    student.style.background = event.target.value;
+
+
+    if (document.getElementById("backTrueStudent").checked) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: "student", color: event.target.value});
+      });
+      
+      
+    }else {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: "student", color: "#0E4F94"});
+      });
+      
+    }
+
+  }); 
+}
 
 
 function updateFirst(event) {
